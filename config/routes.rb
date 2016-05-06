bf04186baa59de7af4c0ef4994514b6e6f40ee58
Shawn Michaud <shawnmne@hotmail.com>
2016-05-06 16:13:18 -0500

    added delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :howls, only: [:index, :new, :create]
+  resources :howls, only: [:index, :new, :create, :destroy]
   resources :wolves, only: [:index, :new, :create]
 
 end
