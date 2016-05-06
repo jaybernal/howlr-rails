ed1f59d922ada8591ec307f4903290c734fe78cf
Shawn Michaud <shawnmne@hotmail.com>
2016-05-06 14:04:00 -0500

    controller tests completed

--- test/controllers/howls_controller_test.rb
+++ test/controllers/howls_controller_test.rb
@@ -1,19 +1,20 @@
 require 'test_helper'
 
 class HowlsControllerTest < ActionController::TestCase
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+   test "should get new" do
+     get :new
+     assert_response :success
+   end
 
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+   test "should get create" do
+     Wolf.create!(name: "big bad", image: "url", description: "ugly")
+     post :create, howl: {text: "abc", wolf_id: Wolf.first.id}
+     assert_redirected_to howls_path
+   end
 
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+   test "should get index" do
+     get :index
+     assert_response :success
+   end
 
 end
